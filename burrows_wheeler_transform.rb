10c30093da42262bbd3e6b39344663e8475dbb8b
nayu <19748682+nayu7u@users.noreply.github.com>
2023-07-04 01:09:51 +0900

    feat: implement bwt encode

--- burrows_wheeler_transform.rb
+++ burrows_wheeler_transform.rb
@@ -1,5 +1,20 @@
 class BurrowsWheelerTransform
-  def initialize(data:)
-    @data = data
+  def encode(data)
+    splited = data.split('')
+
+    matrix = splited
+      .size
+      .times
+      .inject([]) { |r, i| r << splited.rotate(0-i) }
+
+    [matrix.sort.map(&:last).join, matrix.sort.find_index(splited)]
+  end
+
+  def decode(data, n)
+    splited = data.split('')
+    sorted = splited.sort
   end
 end
+
+p BurrowsWheelerTransform.new.encode('cacao')
+p BurrowsWheelerTransform.new.decode('cacao', 3)
