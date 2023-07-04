a5da59ea346f81ed6a50c9f01d4b5f125a69942d
nayu <19748682+nayu7u@users.noreply.github.com>
2023-07-05 00:41:37 +0900

    test: add bwt test

---
+++ burrows_wheeler_transform_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require_relative "burrows_wheeler_transform.rb"
+
+class BurrowsWheelerTransformTest < Minitest::Test
+  def setup
+    @bwt = BurrowsWheelerTransform.new
+  end
+
+  def test_encode
+    assert_equal ["ccoaa", 2], @bwt.encode("cacao")
+  end
+
+  def test_decode
+    assert_equal "cacao", @bwt.decode("ccoaa", 2)
+  end
+end
+
