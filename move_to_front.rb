2e85293c0adddf60d6a53fb10db347e1cb345687
nayu <19748682+nayu7u@users.noreply.github.com>
2023-07-09 01:45:33 +0900

    feat: implement mtf encode

--- move_to_front.rb
+++ move_to_front.rb
@@ -1,5 +1,23 @@
 class MoveToFront
-  def initialize(data:)
-    @data = data
+  def encode(data)
+    table = []
+    encoded = ""
+
+    data.split("") do |char|
+      char_index = table.find_index(char)
+      if char_index.nil?
+        table.insert(0, char)
+        encoded << char 
+      else
+        table.delete_at(char_index)
+        table.insert(0, char)
+        encoded << char_index.to_s
+      end
+    end
+
+    encoded
+  end
+
+  def decode(data)
   end
 end
