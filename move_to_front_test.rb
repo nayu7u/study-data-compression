d6b177a69bb6e8d535f776045efc968e4d08fc8c
nayu <19748682+nayu7u@users.noreply.github.com>
2023-07-09 02:13:33 +0900

    test: add mtf decode test

--- move_to_front_test.rb
+++ move_to_front_test.rb
@@ -9,4 +9,8 @@ class MoveToFrontTest < Minitest::Test
   def test_encode
     assert_equal "ab111c111", @mtf.encode("ababacaca")
   end
+
+  def test_decode
+    assert_equal "ababacaca", @mtf.encode("ab111c111")
+  end
 end
