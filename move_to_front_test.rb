f43da4cba0d6e867de4917c10a463252e10549f7
nayu <19748682+nayu7u@users.noreply.github.com>
2023-07-09 01:45:49 +0900

    test: add mtf encode test

---
+++ move_to_front_test.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require_relative "move_to_front.rb"
+
+class MoveToFrontTest < Minitest::Test
+  def setup
+    @mtf = MoveToFront.new
+  end
+
+  def test_encode
+    assert_equal "ab111c111", @mtf.encode("ababacaca")
+  end
+end
