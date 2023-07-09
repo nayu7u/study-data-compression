7223ad6053b766de0a6cbea0ee514f90537ed64b
nayu <19748682+nayu7u@users.noreply.github.com>
2023-07-09 21:53:00 +0900

    test: fix mtf test

--- move_to_front_test.rb
+++ move_to_front_test.rb
@@ -6,19 +6,27 @@ class MoveToFrontTest < Minitest::Test
     @mtf = MoveToFront.new
   end
 
-  def test_encode
-    assert_equal "ab111c111", @mtf.encode("ababacaca")
+  def test_encode1
+    assert_equal [[97], [0]], @mtf.encode("a")
   end
 
-  def test_decode
-    assert_equal "ababacaca", @mtf.decode("ab111c111")
+  def test_encode2
+    assert_equal [[97, 98], [0, 0, 1, 0]], @mtf.encode("aabb")
+  end
+
+  def test_decode1
+    assert_equal "a", @mtf.decode([97], [0])
+  end
+
+  def test_decode2
+    assert_equal "aabb", @mtf.decode([97, 98], [0, 0, 1, 0])
   end
 
   def test_encode_decode
-    assert_equal "ababacaca", @mtf.decode(@mtf.encode("ababacaca"))
+    assert_equal "ababacaca", @mtf.decode(*@mtf.encode("ababacaca"))
   end
 
   def test_encode_decode_include_number
-    assert_equal "1a1ba11bacaca", @mtf.decode(@mtf.encode("1a1ba11bacaca"))
+    assert_equal "1a1ba11bacaca", @mtf.decode(*@mtf.encode("1a1ba11bacaca"))
   end
 end
