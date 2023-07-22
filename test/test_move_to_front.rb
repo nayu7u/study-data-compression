f10b9d2ec28b4a5d124d030af5c5dfaaff0776e5
nayu <19748682+nayu7u@users.noreply.github.com>
2023-07-23 01:25:28 +0900

    test: fix move_to_front test

--- test/test_move_to_front.rb
+++ test/test_move_to_front.rb
@@ -8,15 +8,15 @@ class MoveToFrontTest < Minitest::Test
   end
 
   def test_encode1
-    assert_equal [0, 1, 0, 2], @mtf.encode([0, 0, 1, 2])
+    assert_equal [0, 1, 0, 2], @mtf.encode([0, 1, 1, 2])
   end
 
   def test_decode1
-    assert_equal [0, 0, 1, 2], @mtf.decode([0, 1, 0, 2])
+    assert_equal [0, 1, 1, 2], @mtf.decode([0, 1, 0, 2])
   end
 
   def test_encode_decode
     random_bytes = SecureRandom.random_bytes(100).bytes
-    assert_equal random_bytes, @mtf.decode(*@mtf.encode(random_bytes))
+    assert_equal random_bytes, @mtf.decode(@mtf.encode(random_bytes))
   end
 end
