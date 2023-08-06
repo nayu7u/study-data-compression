da30e65fada2a838601b772fcdeb3692f681adf8
nayu <19748682+nayu7u@users.noreply.github.com>
2023-08-06 17:16:45 +0900

    test: check zero length encode_decode

--- test/test_run_length_encoding.rb
+++ test/test_run_length_encoding.rb
@@ -26,6 +26,14 @@ class RunLengthEncodingTest < Minitest::Test
     assert_equal [255, 0, 255, 1], @rle.encode([254, 255])
   end
 
+  def test_encode6
+    assert_equal [0, 3], @rle.encode([0, 2])
+  end
+
+  def test_encode7
+    assert_equal [1, 3, 4], @rle.encode([0, 0, 2, 3])
+  end
+
   def test_decode1
     assert_equal [0], @rle.decode([0])
   end
@@ -44,5 +52,23 @@ class RunLengthEncodingTest < Minitest::Test
 
   def test_decode5
     assert_equal [254, 255], @rle.decode([255, 0, 255, 1])
+  end
+
+  def test_decode6
+    assert_equal [0, 2], @rle.decode([0, 3])
+  end
+
+  def test_decode6
+    assert_equal [0, 0, 2, 3], @rle.decode([1, 3, 4])
+  end
+
+  def test_encode_decode1
+    target = [0, 2, 3, 0]
+    assert_equal target, @rle.decode(@rle.encode(target))
+  end
+
+  def test_random_encode_decode
+    target = 10000.times.map { (0..255).to_a.sample }
+    assert_equal target, @rle.decode(@rle.encode(target))
   end
 end
