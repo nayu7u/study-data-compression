914cfdf336ba88b0f4991afe5aabd566fb3b6612
nayu <19748682+nayu7u@users.noreply.github.com>
2023-08-11 20:14:41 +0900

    chore: debug用実装

--- main.rb
+++ main.rb
@@ -4,11 +4,16 @@ require "./run_length_encoding"
 require "./huffman_coding"
 
 class Main
-  def initialize
+  attr_accessor :debug_data
+
+  def initialize(debug = false)
     @bwt = BurrowsWheelerTransform.new
     @mtf = MoveToFront.new
     @rle = RunLengthEncoding.new
     @hc = HuffmanCoding.new
+
+    @debug = debug
+    @debug_data = {}
   end
 
   def encode(bytes)
@@ -17,6 +22,16 @@ class Main
     rle_encoded = @rle.encode(mtf_encoded)
     hex_index = index.to_s(16).split("").map(&:hex)
     size = hex_index.size
+
+    if @debug
+      @debug_data[:encode_size] = size
+      @debug_data[:encode_index] = index
+      @debug_data[:encode_hex_index] = hex_index
+      @debug_data[:encode_rle_encoded] = rle_encoded
+      @debug_data[:encode_mtf_encoded] = mtf_encoded
+      @debug_data[:encode_bwt_encoded] = bwt_encoded
+    end
+
     [size, *hex_index, *rle_encoded]
   end
 
@@ -27,6 +42,16 @@ class Main
     rle_encoded = mixed[size..]
     mtf_encoded = @rle.decode(rle_encoded)
     bwt_encoded = @mtf.decode(mtf_encoded)
+
+    if @debug
+      @debug_data[:decode_size] = size
+      @debug_data[:decode_index] = index
+      @debug_data[:decode_hex_index] = hex_index
+      @debug_data[:decode_rle_encoded] = rle_encoded
+      @debug_data[:decode_mtf_encoded] = mtf_encoded
+      @debug_data[:decode_bwt_encoded] = bwt_encoded
+    end
+
     bytes = @bwt.decode(bwt_encoded, index)
   end
 
