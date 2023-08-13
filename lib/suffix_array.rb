284bdc5b6d6112def9f62a805ff6d01aa12e6152
nayu <19748682+nayu7u@users.noreply.github.com>
2023-08-13 20:36:44 +0900

    wip

--- lib/suffix_array.rb
+++ lib/suffix_array.rb
@@ -1,6 +1,8 @@
 class SuffixArray
   def initialize(bytes)
     @bytes = bytes
+    #@length = @bytes.size - 1
+    @length = @bytes.size
 
     sort
   end
@@ -13,14 +15,29 @@ class SuffixArray
     @sorted.find_index(0)
   end
 
-  private
-
   def sort
-    @length = @bytes.size - 1
-    @sorted = @bytes
-      .size
-      .times
-      .to_a
-      .sort { |i, j| (@bytes*2)[i..i+@length] <=> (@bytes*2)[j..j+@length] }
+    #@sorted = @bytes
+    #  .size
+    #  .times
+    #  .to_a
+    #  .sort { |i, j| (@bytes*2)[i..i+@length] <=> (@bytes*2)[j..j+@length] }
+    
+    # rankの初期値はbyte値とする
+    sa = @bytes.times.to_a
+    rank = @bytes
+
+    # 末尾番兵を用意
+    sa << sa.last + 1
+    rank << -1
+
+    k = 1
+    while k <= @length
+      sa.sort_by! { |sa_i, sa_j|
+        
+      }
+
+      k *= 2
+    end
+
   end
 end
